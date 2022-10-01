49b5afe5d71ce0065198fc460309f62e73f8e85e
Nick <r0883338@student.thomasmore.be>
2022-10-01 22:10:01 +0200

    Project page added, layout navbar
    Layout with footer and navbar automated in app tsx
    project page added to test my layout addition

---
+++ dinowebsite/src/pages/projects.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Projects: NextPage = () => {
+   return (
+     <div>
+       <Head>
+         <title>Dinowebsite | Projects</title>
+         <meta name="description" content="Dinowebsite" />
+         <meta name="robots" content="noindex" />
+         <link rel="icon" href="/favicon.ico" />
+       </Head>
+       </div>
+  );
+};
+
+export default Projects;
