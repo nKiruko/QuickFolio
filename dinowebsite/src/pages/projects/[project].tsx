2934b544db04600f845e0a36b18816068a936db2
Nick <r0883338@student.thomasmore.be>
2022-10-05 00:49:06 +0200

    google tab logo instead of next js favicon

--- dinowebsite/src/pages/projects/[project].tsx
+++ dinowebsite/src/pages/projects/[project].tsx
@@ -33,7 +33,7 @@ const Project: NextPage<ProjectDataProps> = ({projectData}) => {
             <Head>
                 <title>Dinowebsite | { projectData.title }</title>
                 <meta name="description" content="Dinowebsite" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="images/LogoTextTransparant.png" />
             </Head>
             <main className="overflow-hidden">
                 <div className="bg-dinocream pb-24">
