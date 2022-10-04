fa2e77d7fe0088cd6938c8b0a4fa167c50448224
michiel2003 <michieldecap@gmail.com>
2022-10-05 00:48:24 +0200

    + Changed parallax feature on [event].tsx page + Fixed some margins like bottom margint to foorte on [event].tsx + Added a more suiting image
    - removed thomas more image

--- dinowebsite/src/pages/events/[event].tsx
+++ dinowebsite/src/pages/events/[event].tsx
@@ -6,18 +6,18 @@ import fs from "fs";
 import { EventData, getAllEvents, getEventData } from "../../modules/events";
 
 interface EventDataProps {
-    eventData : EventData;
+    eventData: EventData;
 }
 
-export const getStaticProps : GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
     console.log(context?.params?.event);
-    if(!context?.params?.event || context?.params?.event == undefined) throw new Error("Param missing");
+    if (!context?.params?.event || context?.params?.event == undefined) throw new Error("Param missing");
     const eventData = await getEventData(context.params.event);
     console.log(eventData);
     return {
-      props: {
-        eventData
-      }
+        props: {
+            eventData
+        }
     }
 };
 
@@ -25,29 +25,33 @@ export const getStaticPaths = async () => {
     const paths = getAllEvents();
     return {
         paths,
-        fallback : false
+        fallback: false
     }
 }
 
-const Event: NextPage<EventDataProps> = ({eventData}) => {
+const Event: NextPage<EventDataProps> = ({ eventData }) => {
     return (
         <div>
             <Head>
-                <title>Dinowebsite | { eventData.title }</title>
+                <title>Dinowebsite | {eventData.title}</title>
                 <meta name="description" content="Dinowebsite" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="overflow-hidden">
                 <div className="bg-dinocream pb-20">
+                    <header
+                        className="flex items-center justify-center h-[28rem] mb-12 bg-fixed bg-center bg-cover bg-[url('https://images.pexels.com/photos/162500/measurement-millimeter-centimeter-meter-162500.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-no-repeat"
+                    >
+                    </header>
                     <div className="py-10 text-dinoblack flex flex-col justify-center">
-                        <div className="container flex items-center justify-center h-52 md:h-96 m-auto mb-12 bg-fixed bg-center bg-contain bg-no-repeat bg-[url('/images/ThomasMore.png')]">
+                        {/* <div className="container flex items-center justify-center h-52 md:h-96 m-auto mb-12 bg-fixed bg-center bg-contain bg-no-repeat bg-[url('/images/ThomasMore.png')]"> */}
                             {/* <Image src={'/images/ThomasMore.png'} className="rounded-[25px]" width={125} height={250}></Image> */}
-                        </div>
+                        {/* </div> */}
                         <div>
-                            <h1 className="font-heading sm:text-4xl my-4 mx-20">{ eventData.title }</h1>
+                            <h1 className="font-heading sm:text-4xl my-4 mx-20">{eventData.title}</h1>
                         </div>
-                        <div>
-                            <p className="mx-20">{ eventData.text }</p>
+                        <div className="pb-[4rem]">
+                            <p className="mx-20">{eventData.text}</p>
                         </div>
                     </div>
                 </div>
