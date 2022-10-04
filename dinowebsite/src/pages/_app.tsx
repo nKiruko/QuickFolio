51abb09dbc400b5ea7a4a2594f62a6856a4e2ce8
Nick <r0883338@student.thomasmore.be>
2022-10-04 14:17:33 +0200

    lovely footer

--- dinowebsite/src/pages/_app.tsx
+++ dinowebsite/src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 
 import Layout from '../components/Layout'
+import Footerlines from '../components/Footerlines'
 
 export default function MyApp({ Component, pageProps } : any) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Footerlines/>
     </Layout>
   )
 }
--- dinowebsite/src/pages/projects.tsx
+++ dinowebsite/src/pages/projects.tsx
@@ -54,10 +54,7 @@ const Projects: NextPage<AllProjectEntries> = ({ allProjectsData }) => {
              </div>
            </div>
          </div>
-         <div className="mb-[7.5rem]">
-          <div className="bg-tmgrey mb-72 absolute inset-x-0 h-60 rotate-9 scale-125 shadow-glow "></div>
-          <div className="bg-tmorange mb-[9.5rem] absolute inset-x-0 h-60 -rotate-9 scale-125 shadow-glow "></div>
-        </div>
+
        </main>
        </div>
   );
