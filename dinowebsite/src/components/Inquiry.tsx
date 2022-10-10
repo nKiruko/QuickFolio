2a3e8677565c3573b98c8ebafe30482d3d21445b
Nick <r0883338@student.thomasmore.be>
2022-10-10 15:21:51 +0200

    inquiry backend

--- dinowebsite/src/components/Inquiry.tsx
+++ dinowebsite/src/components/Inquiry.tsx
@@ -1,18 +1,21 @@
 import { Icon } from "@iconify/react";
+import { NextPage } from "next";
 import Link from "next/link";
+import { FunctionComponent } from "react";
+import { InquiryData } from "../pages/inquiryPortal";
 
-const Inquiry = () => {
+
+
+const Inquiry: FunctionComponent<{testData: any}> = ({ testData }) => {
     return(
-        <div className="relative md:w-5/6 pb-10">
+        <div className="relative md:w-5/6">
         <input type="checkbox" className="peer absolute top-0 inset-x-0 w-full h-12 opacity-0 z-10 cursor-pointer"/>
-        <div className="bg-dinoblack h-12 w-full pl-5 flex items-center">
-            <h1 className="text-lg font-heading text-white float-left px-2">
-            Sender&#58; john doe
-            </h1>
-            <h1 className="text-lg font-heading text-white float-right px-2 hidden md:block">
-            Email&#58; johndoe@hotmail.com
-            </h1>
-        </div>
+                <div className="bg-dinoblack h-12 w-full pl-5 flex items-center">
+                    <h1 className="text-lg font-heading text-white float-left px-2">Sender&#58; {testData.firstname}</h1>
+                    <h1 className="text-lg font-heading text-white float-right px-2 hidden md:block">
+                    Email&#58; {testData.email}
+                    </h1>
+                </div>
 
         <div className="absolute top-3 right-3 text-white transition-transform duration-400 rotate-0 peer-checked:rotate-180">
             <Icon className="w-6 h-6" icon="material-symbols:arrow-drop-down-circle-outline"/>
@@ -21,14 +24,14 @@ const Inquiry = () => {
         <div className="bg-dinogrey bg-opacity-50 overflow-hidden max-h-0 peer-checked:max-h-full">
             <div className="p-4">
             <p className="font-semibold md:hidden">
-                Email&#58; johndoe@hotmail.com
+                Email&#58; {testData.email}
             </p>
             <p className="font-semibold">
                 Message&#58;
             </p>
-            <p className="pb-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci atque cupiditate necessitatibus eveniet quo quae excepturi neque nam, blanditiis quisquam!</p>
+            <p className="pb-5">{testData.message}</p>
             <Link href="mailto:someone@yoursite.com">
-                <a className="rounded-full border-2 border-dinoblack px-5 hover:border-tmorange float-right">
+                <a className="rounded-full border-2 border-dinoblack px-5 hover:border-tmorange float-right mb-5">
                     Reply
                 </a>
             </Link>
--- dinowebsite/src/pages/inquiryPortal.tsx
+++ dinowebsite/src/pages/inquiryPortal.tsx
@@ -1,9 +1,27 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { Icon } from "@iconify/react";
 import Inquiry from "../components/Inquiry";
 
-const inquiryPortal: NextPage = () => {
+export interface InquiryData {
+    allTestData: GetStaticProps[];
+  }
+  
+  export const getStaticProps: GetStaticProps = async (context) => {
+    const allTestData = [
+        {"firstname": "joe", "email": "joe@hotmail.com", "message": "hello"},
+        {"firstname": "rat", "email": "rat@hotmail.com", "message": "rat"},
+        {"firstname": "banaan", "email": "banaan@hotmail.com", "message": "banaan"},
+    ];
+  
+    return {
+      props: {
+        allTestData,
+      },
+    };
+  };
+  
+const inquiryPortal: NextPage<InquiryData> = ({ allTestData }) => {
     return (
         <div>
             <Head>
@@ -16,7 +34,11 @@ const inquiryPortal: NextPage = () => {
                     <h1 className="font-heading text-3xl sm:text-4xl pt-52 pb-5 text-dinoblack">
                         Question portal
                     </h1>
-                <Inquiry/>
+                    {allTestData.map((inquiry, i) => {
+                        return (
+                            <Inquiry testData={inquiry} key={i}/>
+                        );
+                    })}
                 </div>
             </main>
         </div>
