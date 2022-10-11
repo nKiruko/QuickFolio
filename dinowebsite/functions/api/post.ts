d4346eecc68c12f9f31979d9ddfc016c54775e2c
Syan Delbart <syan.delbart@hotmail.com>
2022-10-11 09:35:16 +0200

    without type declaration

--- dinowebsite/functions/api/post.ts
+++ dinowebsite/functions/api/post.ts
@@ -2,10 +2,10 @@
 // Reacts to POST /hello-world
 export const onRequestPost = async ( request : Request) => {
   const formData = await request.formData();
-  const firstname = formData.get("firstname") as string;
-  const lastname = formData.get("lastname") as string;
-  const email = formData.get("email") as string;
-  const message = formData.get("message") as string;
+  const firstname = formData.get("firstname");
+  const lastname = formData.get("lastname");
+  const email = formData.get("email");
+  const message = formData.get("message");
   //make an object of the above data 
   const data = {
     firstname,
