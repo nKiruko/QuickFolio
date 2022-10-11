3c15fca937909a81583ece39091cc31435264cd3
Syan Delbart <syan.delbart@hotmail.com>
2022-10-11 10:25:18 +0200

    Changed post.ts to make formdata post

--- dinowebsite/functions/api/post.ts
+++ dinowebsite/functions/api/post.ts
@@ -1,29 +1,27 @@
 
-export const onRequestPost = async ( request : Request) => {
+
+export interface requestProps {
+  request : Request;
+}
+
+
+export const onRequestPost: PagesFunction<requestProps> = async ( {request}) => {
   try {
     const { headers } = request;
+    console.log(request);
+    console.log(headers);
     const contentType = headers.get('content-type') || '';
-  
-    if (contentType.includes('application/json')) {
-      return JSON.stringify(await request.json());
-    } else if (contentType.includes('application/text')) {
-      return request.text();
-    } else if (contentType.includes('text/html')) {
-      return request.text();
-    } else if (contentType.includes('form')) {
-      const formData = await request.formData();
-      const body = {};
-      // for (const entry of formData.entries()) {
-      //   body[entry[0]] = entry[1];
-      // }
-      return JSON.stringify(formData);
-    } else {
-      // Perhaps some other type of data was submitted in the form
-      // like an image, or some other binary data.
-      return 'a file';
+
+    if(!contentType.includes('form')) throw new Error('Content-type must be form');
+
+    const formData = await request.formData();
+    const body = {};
+    for (const entry of formData.entries()) {
+      body[entry[0]] = entry[1];
     }
+    return new Response(JSON.stringify(body));
   } catch(e) {
-    let u = e as NodeJS.ErrnoException;
+    let u = e as Error;
     let result = u.message;
     return new Response(result);
   }
