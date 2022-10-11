766a5943a3032895cf428bd52590077cd45cd021
Syan Delbart <syan.delbart@hotmail.com>
2022-10-11 09:31:50 +0200

    post without file

--- dinowebsite/functions/api/post.ts
+++ dinowebsite/functions/api/post.ts
@@ -6,14 +6,12 @@ export const onRequestPost = async ( request : Request) => {
   const lastname = formData.get("lastname") as string;
   const email = formData.get("email") as string;
   const message = formData.get("message") as string;
-  const file = formData.get("file") as File;
   //make an object of the above data 
   const data = {
     firstname,
     lastname,
     email,
-    message,
-    file
+    message
   };
 
   return new Response(JSON.stringify(data));
