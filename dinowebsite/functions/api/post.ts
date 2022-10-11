39399f2f3441aa4404e454daed8c177e65291aa5
Syan Delbart <syan.delbart@hotmail.com>
2022-10-11 09:29:03 +0200

    test post data

--- dinowebsite/functions/api/post.ts
+++ dinowebsite/functions/api/post.ts
@@ -1,5 +1,20 @@
 
 // Reacts to POST /hello-world
 export const onRequestPost = async ( request : Request) => {
-  return new Response("hello world");
+  const formData = await request.formData();
+  const firstname = formData.get("firstname") as string;
+  const lastname = formData.get("lastname") as string;
+  const email = formData.get("email") as string;
+  const message = formData.get("message") as string;
+  const file = formData.get("file") as File;
+  //make an object of the above data 
+  const data = {
+    firstname,
+    lastname,
+    email,
+    message,
+    file
+  };
+
+  return new Response(JSON.stringify(data));
 }
