a36607f268b404a88205007835ebaee5f9225271
Syan Delbart <syan.delbart@hotmail.com>
2022-10-11 12:37:32 +0200

    added content type header

--- dinowebsite/functions/api/post.ts
+++ dinowebsite/functions/api/post.ts
@@ -14,12 +14,19 @@ export const onRequestPost: PagesFunction<requestProps> = async ( {request}) => 
 
     if(!contentType.includes('form')) throw new Error('Content-type must be form');
 
+    
     const formData = await request.formData();
+    return new Response(formData.get("file"), {
+      headers: { "content-type": "application/pdf" },
+    });
+
     const body = {};
     for (const entry of formData.entries()) {
       body[entry[0]] = entry[1];
     }
+
     return new Response(JSON.stringify(body));
+    
   } catch(e) {
     let u = e as Error;
     let result = u.message;
