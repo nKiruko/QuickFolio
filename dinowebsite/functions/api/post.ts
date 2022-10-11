c012164c22014fec4d663ee29e8826b47d99b687
Jonas Claes <jonas@jonasclaes.be>
2022-10-11 13:00:00 +0200

    Fixed Syan's bug

--- dinowebsite/functions/api/post.ts
+++ dinowebsite/functions/api/post.ts
@@ -8,24 +8,24 @@ export interface requestProps {
 export const onRequestPost: PagesFunction<requestProps> = async ( {request}) => {
   try {
     const { headers } = request;
-    console.log(request);
-    console.log(headers);
     const contentType = headers.get('content-type') || '';
 
     if(!contentType.includes('form')) throw new Error('Content-type must be form');
 
-    
     const formData = await request.formData();
-    return new Response(formData.get("file"), {
-      headers: { "content-type": "application/pdf" },
+
+    console.log(formData)
+
+    return new Response(formData.get("pdfke"), {
+      headers: { "Content-Type": "application/pdf" },
     });
 
-    const body = {};
-    for (const entry of formData.entries()) {
-      body[entry[0]] = entry[1];
-    }
+    // const body = {};
+    // for (const entry of formData.entries()) {
+    //   body[entry[0]] = entry[1];
+    // }
 
-    return new Response(JSON.stringify(body));
+    // return new Response(JSON.stringify(body));
     
   } catch(e) {
     let u = e as Error;
