f1906da8bfd4bf6ce026606435d6bb6c38d41eab
Nick <r0883338@student.thomasmore.be>
2022-10-12 12:38:17 +0200

    pages

---
+++ dinowebsite/src/pages/success.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const success: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Digital Innovation - Success</title>
+        <meta name="description" content="Digital Innovation Thomas More, What is Digital Innovation" />
+        <link rel="icon" href="images/Favicon_di.png" />
+      </Head>
+      <main className="text-dinoblack">
+        <div className="mx-20 3xl:pb-24 pb-60">
+          <h1 className="font-heading text-4xl sm:text-5xl pt-24 sm:pt-32 pb-5 ">
+            Success
+          </h1>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default success;
