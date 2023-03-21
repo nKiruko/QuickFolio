526147dc8c2be6272843a5653f88d5764623d57c
Syan Delbart <syan.delbart@hotmail.com>
2023-03-21 15:09:03 +0100

    Turnstile added?

--- dinowebsite/functions/api/post.ts
+++ dinowebsite/functions/api/post.ts
@@ -1,7 +1,17 @@
 export interface PagesEnv {
   INQUIRIES: KVNamespace;
   R2_INQUIRIES: R2Bucket;
+  SECRET_KEY: string;
 }
+
+type siteVerifyResponse = {
+  success: boolean;
+  challenge_ts: string;
+  hostname: string;
+  "error-codes": Array<string>;
+  action: string;
+  cdata: string;
+};
 
 export enum FormDataItem {
   FIRST_NAME = "firstname",
@@ -10,6 +20,7 @@ export enum FormDataItem {
   MESSAGE = "message",
   FILE = "file",
   COMPANY = "companyname",
+  TOKEN = "cf-turnstile-response",
 }
 
 export const onRequestPost: PagesFunction<PagesEnv> = async ({
@@ -25,7 +36,7 @@ export const onRequestPost: PagesFunction<PagesEnv> = async ({
     // Parse the request data.
     const formData = await request.formData();
 
-    // Store current time 
+    // Store current time
     const currentTime = Date.now();
 
     // Check required fields.
@@ -34,6 +45,7 @@ export const onRequestPost: PagesFunction<PagesEnv> = async ({
       FormDataItem.LAST_NAME,
       FormDataItem.EMAIL,
       FormDataItem.MESSAGE,
+      FormDataItem.TOKEN,
     ];
 
     for (const requiredField of requiredFields) {
@@ -43,16 +55,34 @@ export const onRequestPost: PagesFunction<PagesEnv> = async ({
 
     let fileExist = formData.has(FormDataItem.FILE);
 
-    if(fileExist) {
+    if (fileExist) {
       const r2Key = `file-${currentTime}`;
       const file = formData.get(FormDataItem.FILE);
 
-      if(file && file instanceof File) {
+      if (file && file instanceof File) {
         await env.R2_INQUIRIES.put(r2Key, await file.arrayBuffer());
       }
 
       fileExist = !!(fileExist && file);
     }
+
+    // check Turnstile captcha
+    const ip = request.headers.get("CF-Connecting-IP");
+    let formPostData = new FormData();
+    formPostData.append("secret", env.SECRET_KEY);
+    formPostData.append("response", formData.get(FormDataItem.TOKEN) as string);
+    formPostData.append("remoteip", ip);
+
+    const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+    await fetch(url, {
+      body: formData,
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then((outcome: siteVerifyResponse) => {
+        if (!outcome.success)
+          throw new Error("Blocked request, invalid captcha.");
+      });
 
     // Create the object to enter into the KV namespace.
     const data = {
@@ -62,7 +92,7 @@ export const onRequestPost: PagesFunction<PagesEnv> = async ({
       message: formData.get(FormDataItem.MESSAGE),
       company: formData.get(FormDataItem.COMPANY),
       date: currentTime,
-      file: fileExist
+      file: fileExist,
     };
 
     // Generate a key based on the epoch.
@@ -72,7 +102,6 @@ export const onRequestPost: PagesFunction<PagesEnv> = async ({
     await env.INQUIRIES.put(kvKey, JSON.stringify(data));
 
     return Response.redirect("https://digitalinnovation.be/success", 303);
-
   } catch (e) {
     if (e instanceof Error) {
       return new Response(e.message);
--- dinowebsite/src/components/contact/Form.tsx
+++ dinowebsite/src/components/contact/Form.tsx
@@ -1,7 +1,8 @@
 import { Icon } from "@iconify/react";
+import { NextPage } from "next";
 import { useRef, useState } from "react";
 
-export default function Form() {
+const Form: NextPage = () => {
   const inputFileRef = useRef(null);
   const openFiles = () => {
     if (inputFileRef && inputFileRef.current) {
@@ -20,7 +21,18 @@ export default function Form() {
   }
 
   return (
-    <form action="/api/post" method="POST" encType="multipart/form-data" target="_blank" className="my-5 lg:w-4/6">
+    <form
+      action="/api/post"
+      method="POST"
+      encType="multipart/form-data"
+      target="_blank"
+      className="my-5 lg:w-4/6"
+    >
+      <script
+        src="https://challenges.cloudflare.com/turnstile/v0/api.js"
+        async
+        defer
+      ></script>
       <div className="flex">
         <span className="mr-3">Question</span>
         <label
@@ -60,15 +72,19 @@ export default function Form() {
         required
         className="valid:border-green-500 invalid:border-red-500 outline-tmblue dark:outline-coffeecream w-full rounded bg-dinogrey px-2 py-1 mb-3"
       />
-      
-      <div className={`${option ? "block" : "hidden"} flex flex-col lg:flex-row gap-3`}>
+
+      <div
+        className={`${
+          option ? "block" : "hidden"
+        } flex flex-col lg:flex-row gap-3`}
+      >
         <input
           type="text"
           name="companyname"
           placeholder="Company name"
           className="valid:border-green-500 invalid:border-red-500 outline-tmblue dark:outline-coffeecream rounded bg-dinogrey px-2 py-1 w-full mt-3 lg:w-2/4"
         />
-        <p  className=" w-full lg:w-2/4"></p>
+        <p className=" w-full lg:w-2/4"></p>
       </div>
       <textarea
         name="message"
@@ -80,17 +96,37 @@ export default function Form() {
         className="flex gap-3 items-center mb-3 cursor-pointer"
         onClick={() => openFiles()}
       >
-        <input id="fileupload" name="file" type="file" onChange={(e) => {getFileName(e)}} ref={inputFileRef} className="hidden" accept=".doc, .docx, .txt, .pdf, .xlsx, .ppt" />
+        <input
+          id="fileupload"
+          name="file"
+          type="file"
+          onChange={(e) => {
+            getFileName(e);
+          }}
+          ref={inputFileRef}
+          className="hidden"
+          accept=".doc, .docx, .txt, .pdf, .xlsx, .ppt"
+        />
         <div className="p-2 rounded bg-gray-300 text-gray-500">
           <Icon icon="ant-design:plus-outlined" className="text-2xl " />
         </div>
         <p>{file}</p>
-        <Icon icon="entypo:cross" className={`text-2xl hover:text-red-500 ${file === "Add files" ? "hidden" : "block"}`} onClick={(e) => {
-          e.stopPropagation();
-          setFile("Add files");
-          (inputFileRef.current as never as HTMLInputElement).value = "";
-        }} />
+        <Icon
+          icon="entypo:cross"
+          className={`text-2xl hover:text-red-500 ${
+            file === "Add files" ? "hidden" : "block"
+          }`}
+          onClick={(e) => {
+            e.stopPropagation();
+            setFile("Add files");
+            (inputFileRef.current as never as HTMLInputElement).value = "";
+          }}
+        />
       </div>
+      <div
+        className="cf-turnstile"
+        data-sitekey={process.env.NEXT_PUBLIC_SITE_KEY}
+      ></div>
       <button
         type="submit"
         className="bg-dinoblack text-dinocream px-24 py-1 rounded-md border-2 border-tmblue dark:border-coffeegreen"
@@ -99,4 +135,6 @@ export default function Form() {
       </button>
     </form>
   );
-}
+};
+
+export default Form;
